bc5a929039c890fd4cef98e4fcde39c1fbd6a617
onion-40 <Nigella@LAPTOP-VURENRQG.(none)>
2026-08-07 00:43:10 +0800

    k

--- flight-reservation-server/routes/admin-dashboard-routes.js
+++ flight-reservation-server/routes/admin-dashboard-routes.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const adminDashboardController = require("../controllers/adminDashboardController"); 
 const authMiddleware = require("../middleware/auth");
 
-//authenticate
 router.get("/", authMiddleware.isAuthenticated, authMiddleware.isAdmin, adminDashboardController.renderDashboard);
 // revenue
 router.get("/revenue", authMiddleware.isAuthenticated, authMiddleware.isAdmin, adminDashboardController.getRevenueData);
