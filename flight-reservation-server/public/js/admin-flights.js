f0c97125f726f3bb4dec858e6e45667df3389cc7
Rei <SRLagoos@gmail.com>
2026-08-07 01:01:40 +0800

    tf

--- flight-reservation-server/public/js/admin-flights.js
+++ flight-reservation-server/public/js/admin-flights.js
@@ -151,7 +151,7 @@ function applyFilters() {
 
         success: function (flights) {
 
-            console.log("Flights:", flights); // ADDED FOR DEBUG !!
+            console.log("Flights:", flights); // for debugging
 
             filteredFlights = flights;
 
@@ -460,7 +460,7 @@ $(document).ready(function () {
             selectedFlight.flight_number
         );
 
-        console.log("Selected flight ID to delete:", selectedFlightId); //debug
+        console.log("Selected flight ID to delete:", selectedFlightId); // for debuggin
     });
 
     //add flight
@@ -615,7 +615,7 @@ $(document).ready(function () {
 
         if (selectedFlightId === null) { return; }
 
-        console.log("Selected flight ID to delete:", selectedFlightId); //debug
+        console.log("Selected flight ID to delete:", selectedFlightId); // for debugging
 
 
         let modal = new bootstrap.Modal(document.getElementById("deleteModal"));
