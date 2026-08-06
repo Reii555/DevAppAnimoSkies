2c47e1ac1ed370bb86ca0035473c79987b5574f0
Rei <SRLagoos@gmail.com>
2026-08-07 02:11:53 +0800

    aaaa

--- flight-reservation-server/routes/admin-reservations-routes.js
+++ flight-reservation-server/routes/admin-reservations-routes.js
@@ -3,28 +3,8 @@ const router = express.Router();
 const adminReservationController = require('../controllers/adminReservationController');
 const authMiddleware = require('../middleware/auth');
 
-<<<<<<< HEAD
-router.get('/', 
-authMiddleware.isAuthenticated, 
-    authMiddleware.isAdmin, 
-    adminReservationController.renderPage
-);
-
-router.get('/api', 
-    authMiddleware.isAuthenticated, 
-    authMiddleware.isAdmin, 
-    adminReservationController.getReservations
-);
-
-router.put('/:id/status', 
-    authMiddleware.isAuthenticated, 
-    authMiddleware.isAdmin, 
-    adminReservationController.updateStatus
-);
-=======
 router.get('/', authMiddleware.isAuthenticated, authMiddleware.isAdmin, adminReservationController.renderPage);
 router.get('/api', authMiddleware.isAuthenticated, authMiddleware.isAdmin, adminReservationController.getReservations);
 router.put('/:id/status', authMiddleware.isAuthenticated, authMiddleware.isAdmin, adminReservationController.updateStatus);
->>>>>>> 202697c140a1bfba50e61d5e7ea12bbb3c4b0eb1
 
 module.exports = router;
